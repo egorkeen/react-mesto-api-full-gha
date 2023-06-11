f869c752a45dbdee39fd2d49202a5d9103dbea82
Egor Kirienko <egor.keen@mail.ru>
2023-06-11 14:45:13 +0300

    hotfix: fixed problems after 1st review

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
 // импортируем все необходимое
 // express, b-p, mongoose
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const bodyParser = require('body-parser');
 const handleErrors = require('./middlewares/handleErrors');
+
+const { DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 // app
 const app = express();
@@ -36,6 +40,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // подключить cors
 app.use(cors());
 
+// подключить helmet
+app.use(helmet());
+
+// подключить rateLimiter
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+}));
+
 // подключить логгер запросов
 app.use(requestLogger);
 
@@ -52,19 +65,20 @@ app.use(signUpRouter);
 app.use(auth, cardRouter);
 app.use(auth, userRouter);
 
-// подключить логгер ошибок
-app.use(errorLogger);
-
-// отслеживать ошибки
 // создать миддлуэр на случай несуществующей страницы
 app.use(auth, (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
+
+// подключить логгер ошибок
+app.use(errorLogger);
+
+// отслеживать ошибки
 app.use(errors());
 app.use(handleErrors);
 
 // подключиться к базе данных
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose.connect(DB_URL);
 // запустить сервер на порту
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
