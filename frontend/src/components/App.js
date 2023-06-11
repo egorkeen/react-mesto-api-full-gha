c347e054bde0d1e358c78ed78f0dc2248af84832
Egor Kirienko <egor.keen@mail.ru>
2023-06-11 16:22:41 +0300

    fixed problems with likes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -104,7 +104,7 @@ function App() {
   }
   // отследить нажатие кнопки лайка
   function handleCardLike(card) {
-    const isLiked = card.likes.some((i) => i._id === currentUser._id);
+    const isLiked = card.likes.some((i) => i === currentUser._id);
 
     api
       .changeLikeCardStatus(card._id, !isLiked)
